fbaed094505864e31f70038796f7cec02e2cede4
Kevin Moore <moore.kevin.p@gmail.com>
2019-11-18 11:34:47 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+## Google Books API User Stories
+
+1. Provide a search form to allow users to search for books by some search term that they enter.
+2. Allow filters such as type of book or whether the book is a free ebook or not.
+3. Display the search result in list and optionally allow the user to click a result to see further details of the book.
+4. Use the Google Books API to perform the search
+
+
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Search from './Search/Search'
+import Filters from './Filters/Filters'
+import Display from './Display/Display'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <main className='App'>
+        <h1>Google Book Search</h1>
+        <Search />
+        <Filters />
+        <Display />
+      </main>
+    )
+  }
 }
 
 export default App;
